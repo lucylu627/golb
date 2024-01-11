67b2f484c544da413c94b95b87f89c12f3945498
Luby Lu <lucylu627@hotmail.com>
2024-01-12 00:54:35 +0100

    First commit. It works

---
+++ scripts.js
@@ -0,0 +1,54 @@
+var remarkdownCss = document.createElement('link');
+remarkdownCss.href = '/assets/remarkdown.css';
+remarkdownCss.type = 'text/css';
+remarkdownCss.rel = "stylesheet"
+document.head.append(remarkdownCss);
+
+var hljsCss = document.createElement('link');
+hljsCss.href = '/assets/hljs-grayscale.css';
+hljsCss.type = 'text/css';
+hljsCss.rel = 'stylesheet'
+document.head.append(hljsCss);
+
+var customCss = document.createElement('link');
+customCss.href = '/styles.css';
+customCss.type = 'text/css';
+customCss.rel = 'stylesheet'
+document.head.append(customCss);
+
+var mainDom = document.getElementById('main');
+
+var decorate = function (oldDom, newHTML) {
+    oldDom.innerHTML = newHTML;
+    oldDom.className = 'remarkdown h1-underline ul-star em-star strong-star a-showurl code-tick pre-tick';
+    oldDom.style.whiteSpace = '';
+    oldDom.style.fontFamily = '';
+    oldDom.style.maxWidth = '';
+    oldDom.style.margin = '';
+};
+var loadJs = function (url, callback) {
+    var script = document.createElement('script')
+    script.type = 'text/javascript';
+    if (script.readyState) {  //IE
+        script.onreadystatechange = function () {
+            if (script.readyState == 'loaded' ||
+                script.readyState == 'complete') {
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else {  //Others
+        script.onload = function () {
+            callback();
+        };
+    }
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+};
+
+loadJs('/assets/marked.min.js', function () {
+    decorate(mainDom, marked(mainDom.innerText.replace(/&gt;+/g, '>')));
+    loadJs('/assets/highlight.min.js', function() {
+        hljs.highlightAll();
+    });
+});
